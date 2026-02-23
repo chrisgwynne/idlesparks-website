e8a823255ff7261f8e891fb289a56504087b79b5
Chris <chris@idlesparks.com>
2026-02-23 17:50:02 +0000

    Server update: 2026-02-23 17:50:02

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,7 +7,7 @@
   <title><?= $page->title() ?> | <?= $site->title() ?></title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:ital,wght@0,400;0,700;1,400&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
   <?= css('assets/css/main.css') ?>
 </head>
 <body class="bg-navy-900 text-white font-sans antialiased">
